579c4034032059441e66b2a36823d79b09d96291
Albin Baby <albinbabayme05@gmail.com>
2025-03-20 10:32:15 +0100

    findind highest a nd lowest number

---
+++ minMaxNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter Number Coma Separted : ");
+        string[] userInputs = (Console.ReadLine()??" ").Split(',');
+
+        //create the List
+        List<int> numbersList = new List<int>();
+
+        foreach(string num in userInputs){
+            if(int.TryParse(num, out int number)){
+                numbersList.Add(number);
+            }
+        }
+
+        int highestNumber = 0;
+        int lowesrNumber = numbersList[0];
+        foreach(int currentNumber in numbersList){
+            if(currentNumber > highestNumber){
+                highestNumber = currentNumber;
+            }
+            if(currentNumber < lowesrNumber){
+                lowesrNumber= currentNumber;
+            }
+        }
+        Console.WriteLine("The Highest Number is: "+highestNumber);
+        Console.WriteLine("The Lowest Number is: "+lowesrNumber);
+    }
+}
