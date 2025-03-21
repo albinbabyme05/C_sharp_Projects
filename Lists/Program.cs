ab3b0c3cf99483046c7c4e8838cbe80e94efcfbb
Albin Baby <albinbabayme05@gmail.com>
2025-03-21 15:29:28 +0100

    List two basics tutorial added

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -6,22 +6,37 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Enter the number separted by comma ! \n");
-        string[] inputs = (Console.ReadLine()??"").Split(',');
+        // Console.WriteLine("Enter the number separted by comma ! \n");
+        // string[] inputs = (Console.ReadLine()??"").Split(',');
 
-        List<int> numbers = new List<int>();
+        // List<int> numbers = new List<int>();
 
-        foreach(string num in inputs){
-            if(int.TryParse(num, out int number)){
-                numbers.Add(number);
-            }
-        }
+        // foreach(string num in inputs){
+        //     if(int.TryParse(num, out int number)){
+        //         numbers.Add(number);
+        //     }
+        // }
         
-        int sum = 0;
-        foreach(int num in numbers){
-            sum += num;
+        // int sum = 0;
+        // foreach(int num in numbers){
+        //     sum += num;
+        // }
+        // Console.WriteLine("The sum of all number in the List is "+sum);
+
+        //====================================================
+        List<int> numberList = new List<int>();
+        for(int i=0; i<5; i++){
+            Console.WriteLine($"Enter number : ");
+            int num = Convert.ToInt32(Console.ReadLine()??"");
+
+            numberList.Add(num);
         }
-        Console.WriteLine("The sum of all number in the List is "+sum);
+
+        foreach(int num in numberList){
+            Console.Write($"{num} ");
+        }
+
+
 
     }
 }
