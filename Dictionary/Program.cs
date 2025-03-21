3fca5fd40f18fae204b89e4b41c7f10425edb4ad
Albin Baby <albinbabayme05@gmail.com>
2025-03-21 15:28:44 +0100

    syta and basic of dictionary added

---
+++ Dictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<String,long> phoneBook = new Dictionary<string, long>();
+
+        //add key-value to Dicitionary
+        phoneBook.Add("Albin", 9744004873);
+        phoneBook["Unni"] = 8281619237;
+
+
+        //check the ley in the Dicitionary
+        if(phoneBook.ContainsKey("Unni")){
+            Console.WriteLine("Unni number is "+phoneBook["Unni"]);
+        }
+
+        // find teh count in dictionary
+        Console.WriteLine(phoneBook.Count);
+
+    }
+}
