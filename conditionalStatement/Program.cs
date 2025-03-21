84840255da183126e9c27c67fdc3971ecaddcda0
Albin Baby <albinbabayme05@gmail.com>
2025-03-21 05:35:17 +0100

    switch statment problem

--- conditionalStatement/Program.cs
+++ conditionalStatement/Program.cs
@@ -24,5 +24,46 @@ class Program
             Console.WriteLine("Something went Wrong!");
         }
 
+        //====================Switch Statement===========================
+
+        Console.WriteLine("Enter the day of the week: ");
+        string? dayInput = Console.ReadLine();
+        int day = Convert.ToInt32(dayInput);
+
+        switch (day)
+        {
+            case 1:
+            Console.WriteLine("Today is Sunday!!");
+            break;
+
+            case 2:
+            Console.WriteLine("Today is Monday!!");
+            break;
+
+            case 3:
+            Console.WriteLine("Today is Tuesday!!");
+            break;
+
+            case 4:
+            Console.WriteLine("Today is Wednesday!!");
+            break;
+
+            case 5:
+            Console.WriteLine("Today is Thursday!!");
+            break;
+
+            case 6:
+            Console.WriteLine("Today is Friday!!");
+            break;
+
+            case 7:
+            Console.WriteLine("Today is Saturday!!");
+            break;
+
+            default:
+            Console.WriteLine("Invalid Entry !!");
+            break;
+        }
+
     }
 }
