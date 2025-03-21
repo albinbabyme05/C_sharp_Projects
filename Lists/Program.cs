442ae213b3f1f1e20cb3ccd97ee3b11673cdc003
Albin Baby <albinbabayme05@gmail.com>
2025-03-21 16:59:15 +0100

    add odd to odd list and even to even list, number 0-20 range

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -6,22 +6,22 @@ class Program
 {
     static void Main()
     {
-        // Console.WriteLine("Enter the number separted by comma ! \n");
-        // string[] inputs = (Console.ReadLine()??"").Split(',');
+        Console.WriteLine("Enter the number separted by comma ! \n");
+        string[] inputs = (Console.ReadLine()??"").Split(',');
 
-        // List<int> numbers = new List<int>();
+        List<int> numbers = new List<int>();
 
-        // foreach(string num in inputs){
-        //     if(int.TryParse(num, out int number)){
-        //         numbers.Add(number);
-        //     }
-        // }
+        foreach(string num in inputs){
+            if(int.TryParse(num, out int number)){
+                numbers.Add(number);
+            }
+        }
         
-        // int sum = 0;
-        // foreach(int num in numbers){
-        //     sum += num;
-        // }
-        // Console.WriteLine("The sum of all number in the List is "+sum);
+        int sum = 0;
+        foreach(int num in numbers){
+            sum += num;
+        }
+        Console.WriteLine("The sum of all number in the List is "+sum);
 
         //====================================================
         List<int> numberList = new List<int>();
@@ -34,6 +34,33 @@ class Program
 
         foreach(int num in numberList){
             Console.Write($"{num} ");
+        }
+
+        //=============Problem=====================
+        /* create two list with int datatype, one for even and one for odd
+        * loop for 0-20
+            *if number is even add to even list
+            *if number is odd add to odd list
+           *print odd and even list 
+        */
+
+        List<int> oddList = new List<int>();
+        List<int> evenList = new List<int>();
+
+        for(int i=0; i<=20; i++){
+            if(i % 2 == 0){
+                evenList.Add(i);
+            }else{
+                oddList.Add(i);
+            }
+        }
+        Console.WriteLine("Evenlist: ");
+        foreach(int iteam in evenList){
+            Console.Write($"{iteam},");
+        }
+        Console.WriteLine("oddlist: ");
+        foreach(int iteam in oddList){
+            Console.Write($"{iteam},");
         }
 
 
