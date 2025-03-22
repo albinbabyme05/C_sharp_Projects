77440b690ff619959f82006c171dd031996bb38e
Albin Baby <albinbabayme05@gmail.com>
2025-03-22 16:51:24 +0100

    Calculate the sum of the array element

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -99,5 +99,48 @@ class Program
         Console.WriteLine(message);
         return Convert.ToInt32(Console.ReadLine());
     }
+    //================================================================
+        /*
+        * Create and initialise int array of numbers
+        * Create function SumOfNumbers with int return type
+        * int array param
+        * function should return total of all numbers
+        * call in main and output the total
+        * extra: check array length
+        *   return -1 if array empty
+        *   check return in main and output message
+        *   do we need to return -1, how else can we make this?
+        */
+    //================================================================
+    static void Main(){
+        int[] numbers = new int[]{1, 2, 3, 4, 5 , 6};
+
+        int result = FindSum(numbers);
+        if(result > -1){
+            Console.WriteLine($"Sum of the array: {result}");
+        }
+        else{
+            Console.WriteLine("Array is Empty!!!");
+        }
+    }
+
+    static int FindSum(int[] numberList){
+        int sum=0;
+        if(numberList.Length > 0){
+            foreach(int iteam in numberList){
+            sum+=iteam;
+        }
+        return sum;
+        }
+        return -1;
+        
+    }
+
+    //================================================================
+        /* Alternative Way*/
+    //================================================================
+
+
+
 }
 
