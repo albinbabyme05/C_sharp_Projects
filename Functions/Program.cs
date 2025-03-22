910a68a4bcc9d458e84e1bd539585ac7acc25a8c
Albin Baby <albinbabayme05@gmail.com>
2025-03-22 14:03:46 +0100

    finding the index position of an element from the list

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -19,5 +19,26 @@ class Program
         nameInput = "Unni";
         numInput = 20;
     }
+
+    //========================================================
+    /* problem:2  find the index of 'element' from the list */
+    //========================================================
+    static void Main()
+    {
+        List<string> shoppingList = new List<string>
+        {"milk", "coffee", "cup", "tea"};
+
+        // find the index position of 'coffee' from the list.
+        int index = -1; //0-> 
+
+        for(int i=0; i<shoppingList.Count; i++){
+            if(shoppingList[i].ToLower().Equals("tea")){
+                index = i;
+            }
+        }
+        Console.WriteLine(index>-1 ? "found" : "not found");
+        Console.WriteLine("Position: "+index);
+
+    }
 }
 
