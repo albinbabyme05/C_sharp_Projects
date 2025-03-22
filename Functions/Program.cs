93b0cbe33c3e3164556789818871011beff72eea
Albin Baby <albinbabayme05@gmail.com>
2025-03-22 13:49:39 +0100

    function defined with simple problem

---
+++ Functions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "albin";
+        int num = 10;
+
+        Assign(ref name,ref num);
+        Console.WriteLine(name);
+        Console.WriteLine(num);
+
+    }
+
+    static void Assign(ref string nameInput,ref int numInput){
+        nameInput = "Unni";
+        numInput = 20;
+    }
+}
+
